a3e66141189ebede52530ed8b6bc9c4b5bfd4d24
tani <berukoko1220-blog@yahoo.co.jp>
2021-01-18 23:08:05 +0900

    update styles

--- src/components/BlogPages/index.js
+++ src/components/BlogPages/index.js
@@ -2,12 +2,9 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { TwitterShareButton } from "react-share";
 
 const useStyles = makeStyles({
   root: {
@@ -38,13 +35,6 @@ export function BlogPages({ title, Image, subTitle, url }) {
             </Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            <TwitterShareButton url={url} title={title}>
-              Share
-            </TwitterShareButton>
-          </Button>
-        </CardActions>
       </Card>
     </>
   );
