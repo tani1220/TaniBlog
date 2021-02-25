8f690ff896a46d3f9862a8accb2448e355736459
tani <berukoko1220-blog@yahoo.co.jp>
2021-02-25 15:30:42 +0900

    :wrench: Add Google Analytics files.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,21 @@
 import "src/styles/global.css";
+import * as gtag from "src/lib/gtag";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import { ThemeProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider forcedTheme={Component.theme || undefined}>
       <Component {...pageProps} />
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,6 +1,8 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/styles";
 
+import { GA_TRACKING_ID } from "../lib/gtag";
+
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheets();
@@ -32,6 +34,22 @@ export default class MyDocument extends Document {
         <Head>
           <link href="/static/favicon.ico" rel="shortcut icon" />
           <meta charSet="utf-8" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
